bb3c7d335a6b925fa84987351b700bc2b0db91ca
alehhu <159355663+alehhu@users.noreply.github.com>
2024-08-31 21:52:33 +0200

    directory refactoring

--- simple_Ecommerce/wsgi.py
+++ simple_Ecommerce/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for small_Ecommerce project.
+WSGI config for simple_Ecommerce project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'small_Ecommerce.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simple_Ecommerce.settings')
 
 application = get_wsgi_application()
